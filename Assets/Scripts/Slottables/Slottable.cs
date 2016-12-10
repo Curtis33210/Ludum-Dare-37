7e30d31e4723b62491e169e374208f69bcadee72
Nimurai <k.harui.philp@gmail.com>
2016-12-10 20:26:31 +1000

    Merge remote-tracking branch 'refs/remotes/origin/master' into Mine
    # Conflicts:
    #	Assets/Mats/Purp!3.mat
    #	Assets/PREFAB/Square Room.prefab
    #	Assets/Scenes/Kotaro.unity

---
+++ Assets/Scripts/Slottables/Slottable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slottable : MonoBehaviour {
+
+    [SerializeField]
+    private SlottableType _type;
+    public SlottableType Type { get { return _type; } }
+
+}
