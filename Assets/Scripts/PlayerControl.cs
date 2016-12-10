1bb18688b151248506496c36ddb786c19cd2d154
Nimurai <k.harui.philp@gmail.com>
2016-12-10 18:32:37 +1000

    Movement finish and camera look finish

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -11,25 +11,29 @@ public class PlayerControl : MonoBehaviour
     }
     void FixedUpdate()
     {
-        Vector3 _forward = new Vector3(0, 0, 1 );
-        Vector3 _backward = new Vector3(0, 0, -1);
-        Vector3 _left = new Vector3(-1, 0, 0);
-        Vector3 _right = new Vector3(1, 0, 0);
+        Vector3 _forward = transform.forward;
+        Vector3 _backward = -transform.forward;
+        Vector3 _left = -transform.right;
+        Vector3 _right = transform.right;
+        Vector3 _final = new Vector3(0, 0, 0);
         if ((Input.GetKey(KeyCode.W) || (Input.GetKey(KeyCode.UpArrow))))
         {
-            rb.AddForce(_forward * 10);
+            _final += _forward;
         }
-        else if ((Input.GetKey(KeyCode.S)) || (Input.GetKey(KeyCode.DownArrow)))
+        if ((Input.GetKey(KeyCode.S)) || (Input.GetKey(KeyCode.DownArrow)))
         {
-            rb.AddForce(_backward * 10);
+            _final += _backward;
         }
-        else if ((Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.LeftArrow)))
+        if ((Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.LeftArrow)))
         {
-            rb.AddForce(_left * 10);
+            _final += _left;
         }
-        else if ((Input.GetKey(KeyCode.D)) || (Input.GetKey(KeyCode.RightArrow)))
+        if ((Input.GetKey(KeyCode.D)) || (Input.GetKey(KeyCode.RightArrow)))
         {
-            rb.AddForce(_right * 10);
+            _final += _right;
         }
+        _final = new Vector3(_final.x, 0, _final.z);
+        _final.Normalize();
+        rb.MovePosition((transform.position + _final*.2f));
     }
 }
